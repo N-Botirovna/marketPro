33ea92946fcd1a2c0132838650c98e51d92c6b60
N-Botirovna <nargizatosheva20@gmail.com>
2025-10-30 12:08:49 +0500

    commit message

---
+++ src/app/[locale]/become-seller/page.jsx
@@ -0,0 +1,12 @@
+import BecomeSellerClient from "./BecomeSellerClient";
+
+export const metadata = {
+  title: "Kitobzor",
+  description: "Kitobzor shiori",
+};
+
+const page = () => {
+  return <BecomeSellerClient />;
+};
+
+export default page;
