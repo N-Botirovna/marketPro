4b44f5fec7a15cf5cae26bbddb175e022f7cc522
N-Botirovna <nargizatosheva20@gmail.com>
2025-11-10 18:01:25 +0500

    commit message

--- src/components/FooterOne.jsx
+++ src/components/FooterOne.jsx
@@ -13,214 +13,159 @@ const FooterOne = () => {
         className="body-bottom-bg"
       />
       <div className="container container-lg">
-        <div className="footer-item-wrapper d-flex align-items-start flex-wrap">
-          <div className="footer-item">
-            <div className="footer-item__logo">
-              <Link href="/">
-                {" "}
-                <img src="assets/images/logo1.png" alt="" />
-              </Link>
-            </div>
-            <p className="mb-24">{tF("about.blurb")}</p>
-            <div className="flex-align gap-16 mb-16">
-              <span className="w-32 h-32 flex-center rounded-circle bg-main-600 text-white text-md flex-shrink-0">
-                <i className="ph-fill ph-map-pin" />
-              </span>
-              <span className="text-md text-gray-900 ">
-                {tF("about.address")}
-              </span>
-            </div>
-            <div className="flex-align gap-16 mb-16">
-              <span className="w-32 h-32 flex-center rounded-circle bg-main-600 text-white text-md flex-shrink-0">
-                <i className="ph-fill ph-phone-call" />
-              </span>
-              <div className="flex-align gap-16 flex-wrap">
+        <div className="row gy-4">
+          <div className="col-lg-4 col-md-6">
+            <div className="footer-item">
+              <div className="footer-item__logo mb-20">
+                <Link href="/">
+                  <img src="assets/images/logo1.png" alt="" />
+                </Link>
+              </div>
+              <p className="mb-20 text-gray-700">{tF("about.blurb")}</p>
+              <div className="flex-align gap-12 mb-12">
+                <span className="w-32 h-32 flex-center rounded-circle bg-main-500 text-white text-md flex-shrink-0">
+                  <i className="ph-fill ph-map-pin" />
+                </span>
+                <span className="text-sm text-gray-700">
+                  {tF("about.address")}
+                </span>
+              </div>
+              <div className="flex-align gap-12 mb-12">
+                <span className="w-32 h-32 flex-center rounded-circle bg-main-500 text-white text-md flex-shrink-0">
+                  <i className="ph-fill ph-phone-call" />
+                </span>
                 <a
-                  href="tel:+00123456789"
-                  className="text-md text-gray-900 hover-text-main-600"
+                  href="tel:+998938340103"
+                  className="text-sm text-gray-700 hover-text-main-500"
                 >
                   +998 93 834 01 03
                 </a>
-                <span className="text-md text-main-600 ">{tF("about.or")}</span>
+              </div>
+              <div className="flex-align gap-12 mb-12">
+                <span className="w-32 h-32 flex-center rounded-circle bg-main-500 text-white text-md flex-shrink-0">
+                  <i className="ph-fill ph-paper-plane-tilt" />
+                </span>
                 <a
-                  href="tel:+00987654012"
-                  className="text-md text-gray-900 hover-text-main-600"
+                  href="https://t.me/kitobzoruz_bot"
+                  className="text-sm text-gray-700 hover-text-main-500"
                 >
-                  +00 987 654 012
+                  {tF("about.telegram")}
+                </a>
+              </div>
+              <div className="flex-align gap-12 mb-0">
+                <span className="w-32 h-32 flex-center rounded-circle bg-main-500 text-white text-md flex-shrink-0">
+                  <i className="ph-fill ph-envelope" />
+                </span>
+                <a
+                  href="mailto:kitobzor.help@gmail.com"
+                  className="text-sm text-gray-700 hover-text-main-500"
+                >
+                  {tF("about.email")}
                 </a>
               </div>
             </div>
-            <div className="flex-align gap-16 mb-16">
-              <span className="w-32 h-32 flex-center rounded-circle bg-main-600 text-white text-md flex-shrink-0">
-                <i className="ph-fill ph-envelope" />
-              </span>
-              <a
-                href="https://t.me/kitobzoruz_bot"
-                className="text-md text-gray-900 hover-text-main-600"
-              >
-                {tF("about.telegram")}
-              </a>
+          </div>
+          <div className="col-lg-2 col-md-6">
+            <div className="footer-item">
+              <h6 className="footer-item__title mb-20">{tF("info.title")}</h6>
+              <ul className="footer-menu">
+                <li className="mb-12">
+                  <Link
+                    href="/about-us"
+                    className="text-sm text-gray-600 hover-text-main-500 transition-1"
+                  >
+                    {tF("info.aboutUs")}
+                  </Link>
+                </li>
+                <li className="mb-12">
+                  <Link
+                    href="/shop"
+                    className="text-sm text-gray-600 hover-text-main-500 transition-1"
+                  >
+                    {tF("info.privacy")}
+                  </Link>
+                </li>
+                <li className="mb-0">
+                  <Link
+                    href="/#faq"
+                    className="text-sm text-gray-600 hover-text-main-500 transition-1"
+                  >
+                    {tF("info.faq")}
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="flex-align gap-16 mb-16">
-              <span className="w-32 h-32 flex-center rounded-circle bg-main-600 text-white text-md flex-shrink-0">
-                <i className="ph-fill ph-envelope" />
-              </span>
-              <a
-                href="mailto:kitobzor.help@gmail.com"
-                className="text-md text-gray-900 hover-text-main-600"
-              >
-                {tF("about.email")}
-              </a>
+          </div>
+          <div className="col-lg-2 col-md-6">
+            <div className="footer-item">
+              <h6 className="footer-item__title mb-20">{tF("help.title")}</h6>
+              <ul className="footer-menu">
+                <li className="mb-12">
+                  <Link
+                    href="/contact"
+                    className="text-sm text-gray-600 hover-text-main-500 transition-1"
+                  >
+                    {tF("help.contact")}
+                  </Link>
+                </li>
+                <li className="mb-0">
+                  <Link
+                    href="/become-seller"
+                    className="text-sm text-gray-600 hover-text-main-500 transition-1"
+                  >
+                    {tF("help.becomeSeller")}
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="footer-item">
-            <h6 className="footer-item__title">{tF("info.title")}</h6>
-            <ul className="footer-menu">
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("info.becomeSeller")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("info.aboutUs")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("info.privacy")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("info.faq")}
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-item">
-            <h6 className="footer-item__title">{tF("categories.title")}</h6>
-            <ul className="footer-menu">
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("categories.religious")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("categories.fiction")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("categories.children")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("categories.education")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("categories.science")}
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-item">
-            <h6 className="footer-item__title">{tF("help.title")}</h6>
-            <ul className="footer-menu">
-              <li className="mb-16">
-                <Link
-                  href="/contact"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("help.contact")}
-                </Link>
-              </li>
-              <li className="mb-16">
-                <Link
-                  href="/shop"
-                  className="text-gray-600 hover-text-main-600"
-                >
-                  {tF("help.becomeSeller")}
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-item">
-            <h6 className="">{tF("newsletter.title")} </h6>
-            <p className="mb-16">{tF("newsletter.follow")}</p>
-            <div className="flex-align gap-8 my-32">
-              <a href="https://www.apple.com/store" className="">
-                <img src="assets/images/thumbs/store-img1.png" alt="" />
-              </a>
-              <a href="https://play.google.com/store/apps?hl=en" className="">
-                <img src="assets/images/thumbs/store-img2.png" alt="" />
-              </a>
+          <div className="col-lg-4 col-md-6">
+            <div className="footer-item">
+              <h6 className="footer-item__title mb-20">{tF("newsletter.title")}</h6>
+              <p className="mb-20 text-sm text-gray-700">{tF("newsletter.follow")}</p>
+              <div className="flex-align gap-8 mb-24">
+                <a href="https://www.apple.com/store" className="hover-opacity-80 transition-1">
+                  <img src="assets/images/thumbs/store-img1.png" alt="" />
+                </a>
+                <a href="https://play.google.com/store/apps?hl=en" className="hover-opacity-80 transition-1">
+                  <img src="assets/images/thumbs/store-img2.png" alt="" />
+                </a>
+              </div>
+              <ul className="flex-align gap-12">
+                <li>
+                  <a
+                    href="https://www.facebook.com"
+                    className="w-40 h-40 flex-center bg-main-50 text-main-500 text-lg rounded-circle hover-bg-main-500 hover-text-white transition-1"
+                  >
+                    <i className="ph-fill ph-facebook-logo" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.twitter.com"
+                    className="w-40 h-40 flex-center bg-main-50 text-main-500 text-lg rounded-circle hover-bg-main-500 hover-text-white transition-1"
+                  >
+                    <i className="ph-fill ph-twitter-logo" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com"
+                    className="w-40 h-40 flex-center bg-main-50 text-main-500 text-lg rounded-circle hover-bg-main-500 hover-text-white transition-1"
+                  >
+                    <i className="ph-fill ph-instagram-logo" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.pinterest.com"
+                    className="w-40 h-40 flex-center bg-main-50 text-main-500 text-lg rounded-circle hover-bg-main-500 hover-text-white transition-1"
+                  >
+                    <i className="ph-fill ph-linkedin-logo" />
+                  </a>
+                </li>
+              </ul>
             </div>
-            <ul className="flex-align gap-16">
-              <li>
-                <a
-                  href="https://www.facebook.com"
-                  className="w-44 h-44 flex-center bg-main-100 text-main-600 text-xl rounded-circle hover-bg-main-600 hover-text-white"
-                >
-                  <i className="ph-fill ph-facebook-logo" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.twitter.com"
-                  className="w-44 h-44 flex-center bg-main-100 text-main-600 text-xl rounded-circle hover-bg-main-600 hover-text-white"
-                >
-                  <i className="ph-fill ph-twitter-logo" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com"
-                  className="w-44 h-44 flex-center bg-main-100 text-main-600 text-xl rounded-circle hover-bg-main-600 hover-text-white"
-                >
-                  <i className="ph-fill ph-instagram-logo" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.pinterest.com"
-                  className="w-44 h-44 flex-center bg-main-100 text-main-600 text-xl rounded-circle hover-bg-main-600 hover-text-white"
-                >
-                  <i className="ph-fill ph-linkedin-logo" />
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
